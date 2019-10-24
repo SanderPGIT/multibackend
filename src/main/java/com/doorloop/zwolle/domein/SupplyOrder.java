f3079d826108dc46a25a1a9870784d7a4ca7ca9d
richardhartemink <richardhartemink@gmail.com>
2019-10-24 17:20:32 +0200

    commit donderdagmiddag

---
+++ src/main/java/com/doorloop/zwolle/domein/SupplyOrder.java
@@ -0,0 +1,49 @@
+package com.doorloop.zwolle.domein;
+
+
+
+import javax.persistence.*;
+
+@Entity
+public class SupplyOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    long id;
+    int Amount;
+    boolean Delivered;
+    @OneToOne
+    Supplier supplier;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(int amount) {
+        Amount = amount;
+    }
+
+    public boolean isDelivered() {
+        return Delivered;
+    }
+
+    public void setDelivered(boolean delivered) {
+        Delivered = delivered;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+}
