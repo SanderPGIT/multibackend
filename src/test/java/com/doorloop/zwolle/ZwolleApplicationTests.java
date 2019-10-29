6e61ae4a9b3b46ad980327270694c98dfdee0890
richardhartemink <richardhartemink@gmail.com>
2019-10-29 14:59:47 +0100

    commit dinsdag 1

--- src/test/java/com/doorloop/zwolle/ZwolleApplicationTests.java
+++ src/test/java/com/doorloop/zwolle/ZwolleApplicationTests.java
@@ -2,12 +2,16 @@ package com.doorloop.zwolle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class ZwolleApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int a = 3;
+		int b = 5;
+		Assert.isTrue(a==b);
 	}
 
 }
